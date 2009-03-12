5bfa5615e44bdcc8b2d789bb0a4795e26bceab3e
bigbear <bigbear@478a9d2e-6004-0410-ac81-b4d184805fe2>
2009-03-12 21:14:57 +0000

    updated the pcp and barchart spaces and barchart functions
    git-svn-id: svn://ggobi.org/ggobi/interface/DescribeDisplay/trunk/DescribeDisplay@6002 478a9d2e-6004-0410-ac81-b4d184805fe2

--- R/ggplot-barchart.r
+++ R/ggplot-barchart.r
@@ -11,27 +11,27 @@
 ggplot.histogram <- function(data, spine = FALSE,...) {
   cat("\nggplot.barchart\n")
 
-	print(head(data$points))
-	#print(unique(data$points$col))
-	
+  print(head(data$points))
+  #print(unique(data$points$col))
+  
   p <- ggplot(data$points, 
-    	aes(x = x, fill = col,...)) +
+      aes(x = x, fill = col,...)) +
     scale_size_identity() + 
     scale_shape_identity() + 
     scale_linetype_identity()# +
-	#scale_fill_identity()
-	
-	
-#	if(spine){
-#		## not correct yet
-#		p <- p + geom_bar( position = "fill", binwidth = diff(data$params$breaks[1:2]) , ...)
-#		cat("\nspine\n")
-#	}else{
-		p <- p + geom_bar(binwidth = diff(data$params$breaks[1:2]),...)
-#	}
+  #scale_fill_identity()
+  
+  
+#  if(spine){
+#    ## not correct yet
+#    p <- p + geom_bar( position = "fill", binwidth = diff(data$params$breaks[1:2]) , ...)
+#    cat("\nspine\n")
+#  }else{
+    p <- p + geom_bar(binwidth = diff(data$params$breaks[1:2]),...)
+#  }
 
-	
-	    
+  
+      
 
 
   p
@@ -49,27 +49,27 @@ ggplot.histogram <- function(data, spine = FALSE,...) {
 #' @examples
 #' ggplot(dd_example("barchart-species"))
 ggplot.barchart <- function(data, spine = FALSE,...) {
-	panel = data$plots[[1]]
+  panel = data$plots[[1]]
 print(panel$points)
-	xCount = table(panel$points$col,panel$points$x)
+  xCount = table(panel$points$col,panel$points$x)
 
-	colnames(xCount) = panel$params$levelnames
-print(xCount)	
-	foregroundColors = data$colormap$foreground
+  colnames(xCount) = panel$params$levelnames
+print(xCount)  
+  foregroundColors = data$colormap$foreground
 print(foregroundColors)
-	
-	uniColors = as.character(unique(panel$points$col));
+  
+  uniColors = as.character(unique(panel$points$col));
 cat("\nuniColors\n");print(uniColors)
-	
+  
 cat("\nnames\n");print(colnames(xCount))
-	
+  
 
-	barplot(xCount,col=uniColors,names.arg=colnames(xCount),main = data$title)
+  barplot(xCount,col=uniColors,names.arg=colnames(xCount),main = data$title)
 
-	
-#	if (all(!is.null(data$params$levelnames) && !is.null(data$params$levelvalues) {
- #   	p <- p + geom_text(aes_string(label = "label"), data=data$labels, justification=c(data$labels$left[1], data$labels$top[1]))
-#	}  
+  
+#  if (all(!is.null(data$params$levelnames) && !is.null(data$params$levelvalues) {
+ #     p <- p + geom_text(aes_string(label = "label"), data=data$labels, justification=c(data$labels$left[1], data$labels$top[1]))
+#  }  
 
-	
-}			
+  
+}      
