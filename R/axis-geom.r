64b6b4fc106f89568e41ed28d53b8b7cfd6a08b1
hadley <h.wickham@gmail.com>
2014-12-05 09:01:59 -0600

    Import proto::proto

--- R/axis-geom.r
+++ R/axis-geom.r
@@ -2,30 +2,31 @@ GeomAxis <- proto(ggplot2:::Geom, {
   draw <- function(., data, scales, coordinates, location = c(0.2, 0.2), size=0.9, colour = "black", axis, ...) {
     axesVp <- axesViewport(axis, location)
     axes <- axesGrob(axis, gp=gpar(col = colour))
-    
+
     gTree(
-      children = gList(axes), 
+      children = gList(axes),
       childrenvp = axesVp
-    )    
+    )
   }
 
   objname <- "axis"
   desc <- "Projection axes"
-  
+
   default_stat <- function(.) StatIdentity
   required_aes <- c()
   default_aes <- function(.) aes()
-  
+
 })
 
 #' Geom Axis.
 #'
 #' A special ggplot2 geom for drawing the tour axes
-#' 
+#'
 #' @param ... should include data, location, aes\_string information
 #' @author Hadley Wickham \email{h.wickham@@gmail.com}
 #' @keywords internal
 #' @export
+#' @importFrom proto proto
 #' @examples
 #' library(ggplot2)
 #' print(ggplot(dd_example("tour2d")))
